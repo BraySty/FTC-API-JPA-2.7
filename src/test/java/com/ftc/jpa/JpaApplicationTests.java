407b2b723720ee82faeba467ee5deffc78956259
BraySty <BraySty>
2024-04-05 11:58:59 +0100

    First commit

---
+++ src/test/java/com/ftc/jpa/JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftc.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
